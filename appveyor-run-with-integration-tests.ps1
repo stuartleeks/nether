43cc80edb67ad7b51b733f6b0581ea16210afba7
Stuart Leeks <stuart.leeks@microsoft.com>
2017-05-23 15:40:48 +0100

    Update output for integration tests

--- appveyor-run-with-integration-tests.ps1
+++ appveyor-run-with-integration-tests.ps1
@@ -64,10 +64,10 @@ function RenameWithRetry($filename, $newfilename) {
         catch {
             Write-Host "Rename failed... retrying"
         }
-        Start-Sleep -Seconds 1
+        Start-Sleep -Seconds 5
         $count += 1
     }
-    Write-Host "Rename faild!"
+    Write-Host "Rename failed!"
     exit -100
 }
 Write-Host "***************************************************************************"
@@ -84,11 +84,12 @@ Write-Host "********************************************************************
 Write-Host "******************"
 Write-Host "*** Running integration tests: in-memory"
 $status = RunIntegrationTests
+Write-Host "*** Tests completed with status '$status'"
 
 # kill the web server (brute force - killing all dotnet processes!)
 Stop-Process -Name dotnet
 RenameWithRetry "$netherRoot\nether.web.log" "$netherRoot\nether.web-inmemory.log"
-Push-AppveyorArtifact "$netherRoot\nether.web-inmemory.log" -FileName nether.web-inmemory.txt
+# Push-AppveyorArtifact "$netherRoot\nether.web-inmemory.log" -FileName nether.web-inmemory.txt
 
 if ($status -ne 0) {
     Write-Host "Exiting with status $status"
@@ -100,6 +101,7 @@ ConfigureSqlServer
 Write-Host "******************"
 Write-Host "*** Running integration tests: SQL Server"
 $status = RunIntegrationTests
+Write-Host "*** Tests completed with status '$status'"
 
 # kill the web server (brute force - killing all dotnet processes!)
 Stop-Process -Name dotnet
